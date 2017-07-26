8b024ff59e2748983e54e8ef56693c2046fab78d
nmerris <nate.merris@gmail.com>
2017-07-26 09:07:40 -0400

    Add sample output to README.md and finished main java file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 This program calculates the cost of fencing based on various user inputs.  The final cost is displayed.
+
+SAMPLE OUTPUT
+
+Please enter yard width: 
+100
+Please enter yard length: 
+50
+What type of fencing? (W for wooden, C for chain link)
+cast iron
+Please enter either W or C
+What type of fencing? (W for wooden, C for chain link)
+c
+How many gates?
+99
+You must have between 1 and 3 gates
+How many gates?
+3
+SALES ESTIMATE
+Perimeter of yard: 300
+Number of gates: 3
+Type of fencing: Chain Link
+Sales tax: $297.00
+Final cost: $5297.00
--- src/fencepackage/CalculateFencingCost.java
+++ src/fencepackage/CalculateFencingCost.java
@@ -10,8 +10,8 @@ public class CalculateFencingCost {
 		
 		int length; // length of yard
 		int width; // width of yard
-		int perimeter; // perimeter of yard, gates are considered to have zero length
-		int numGates;
+		int perimeter; // perimeter of yard, gates are considered to have zero length, assume a perfect rectangle
+		int numGates; // total number of gates, must be 1-3
 		String fenceType = null; // wooden or chain link only
 		int costPerFoot; // depends on fenceType
 		int buildingPermitCost = 50; // always $50
@@ -22,29 +22,49 @@ public class CalculateFencingCost {
 		
 		System.out.println("Please enter yard width: ");
 		width = scanner.nextInt();
-		scanner.nextLine();
+		scanner.nextLine(); // consume the dangling \n
 		
 		System.out.println("Please enter yard length: ");
 		length = scanner.nextInt();
 		scanner.nextLine();
 		
-		System.out.println("What type of fencing? (W for wooden, C for chain link)");
-		fenceType = scanner.nextLine();
+		do {
+			System.out.println("What type of fencing? (W for wooden, C for chain link)");
+			fenceType = scanner.nextLine();
+			// check that user entered W, w, C, or c
+			// if not, display message and re-prompt
+			if(!fenceType.equalsIgnoreCase("w") && !fenceType.equalsIgnoreCase("c"))
+				System.out.println("Please enter either W or C");
+		} while(!fenceType.equalsIgnoreCase("w") && !fenceType.equalsIgnoreCase("c"));
 		
 		do {
 			System.out.println("How many gates?");
 			numGates = scanner.nextInt();
 			scanner.nextLine();
-			if(numGates < 1 || numGates > 3) {
+			if(numGates < 1 || numGates > 3)
 				System.out.println("You must have between 1 and 3 gates");
-			}
 		} while(numGates < 1 || numGates > 3);
 		
 		
+		// crunch the numbers
+		perimeter = 2 * width + 2 * length;
+		costPerFoot = fenceType.equalsIgnoreCase("w") ? 25 : 15; // $25 for wooden, $15 for chain link
+		subTotal = costPerFoot * perimeter + numGates * 150;
+		taxAmount = subTotal * taxRate;
+		finalCost = subTotal + taxAmount + buildingPermitCost;
 		
+		// display a sales estimate
+		System.out.println("SALES ESTIMATE");
+		System.out.println("Perimeter of yard: " + perimeter);
+		System.out.println("Number of gates: " + numGates);
 		
-		perimeter = 2 * width + 2 * length; // always a rectangle
+		// get the full word for fence type, then display it as part of the sales report
+		String fenceTypeForDisplay = fenceType.equalsIgnoreCase("w") ? "Wooden" : "Chain Link";
+		System.out.println("Type of fencing: " + fenceTypeForDisplay);
 		
-	}
-
+		System.out.printf("Sales tax: $%.2f\n", taxAmount);
+		System.out.printf("Final cost: $%.2f\n", finalCost);
+		
+	} // main
+	
 }
